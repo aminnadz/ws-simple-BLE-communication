72e38a27e3ecd3602a812c43cc874cc632e85965
makefitter <31683512+makefitter@users.noreply.github.com>
2018-01-27 13:58:34 +0300

    Serial passthrough Arduino Program
    check readme

---
+++ serial_passthrough.ino
@@ -0,0 +1,25 @@
+#include <Arduino.h>                       
+
+void setup() {
+  SerialUSB.begin(115200);                  /*Initialize USB serial*/
+  
+  while (!SerialUSB) {                      /*Wait for initialization to end*/
+    ;
+  }
+  
+  Serial2.begin(115200);                    /*Initialize Bluetooth Serial*/
+  
+  while (!Serial2) {                        /*Wait for initialization to end*/
+    ;
+  }
+  
+  pinMode(LED_BUILTIN, OUTPUT);             /*Initialize pin for GREEN (D13) led*/
+}
+
+void loop() {
+    if (Serial2.available())                /*Serial PassThrough Loop*/
+      SerialUSB.write(Serial2.read());      /*Read the Bluetooth response and send to USB*/
+       
+    if (SerialUSB.available())              
+      Serial2.write(SerialUSB.read());      /*Read USB command and send to Bluetooth*/
+}
